a80c6d8de84bf49deb4299f2fd1e248881a48763
DKM24 <daud.k.marwat24@gmail.com>
2023-10-21 18:25:42 +0500

    Defining function

---
+++ airDistance.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+#define EARTH_RADIUS 6371.0
+#define M_PI 3.14159265358979323846
+
+double degreesToRadians(double degrees) {
+    return degrees * M_PI/180;
+}
