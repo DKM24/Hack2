ad0693827271e0f09667de15e937bbce56dcf2a8
DKM24 <daud.k.marwat24@gmail.com>
2023-10-21 18:28:58 +0500

    Defining variables and I/O

--- airDistance.c
+++ airDistance.c
@@ -6,3 +6,14 @@
 double degreesToRadians(double degrees) {
     return degrees * M_PI/180;
 }
+
+int main() {
+    double latitude1, longitude1, latitude2, longitude2;
+
+    printf("Enter the latitude and longitude of the origin (in degrees):\n");
+    printf("Latitude:  ");
+    scanf("%lf", &latitude1);
+    printf("Longitude  :");
+    scanf("%lf", &longitude1);
+    
+}
